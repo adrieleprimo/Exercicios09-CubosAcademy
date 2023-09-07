479204729796fda1034bdae8e247e67215ad6f46
Adriele Primo <adriele.dejprimo@gmail.com>
2023-09-07 17:35:52 -0300

    Exercicio sobre maior número realizado

---
+++ 03-maiorNúmero/index.js
@@ -0,0 +1,10 @@
+const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44];
+
+const maiorNúmero = numeros.reduce((acumulador, elementoAtual, indice, array)=>{
+    let maior = acumulador;
+    if(elementoAtual > maior){
+        maior = elementoAtual;
+    }
+    return maior;
+});
+console.log(maiorNúmero);
